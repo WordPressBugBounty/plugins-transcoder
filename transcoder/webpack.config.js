dd011c3f16d8f79a1c4c17b36ccf717732f3c886
Sean Murphy <sean@iamseanmurphy.com>
2025-04-18 12:19:23 +0000

    1.3.8

---
+++ transcoder/webpack.config.js
@@ -0,0 +1,84 @@
+/**
+ * Webpack configuration file.
+ *
+ * @package transcoder
+ */
+
+/* global process */
+
+const webpack = require( 'webpack' );
+const glob = require( 'glob' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
+
+const externals = {
+	react: 'React',
+	'react-dom': 'ReactDOM',
+	'react-dom/server': 'ReactDOMServer',
+	tinymce: 'tinymce',
+	moment: 'moment',
+	jquery: 'jQuery',
+	'@wordpress/components': 'wp.components' // Not really a package.
+};
+
+module.exports = [
+	{
+		entry: {
+			blocks: glob.sync( './admin/js/rt-transcoder-block-editor-support.js' )
+		},
+		output: {
+			filename: './admin/js/build/rt-transcoder-block-editor-support.build.js',
+			path: __dirname
+		},
+		externals,
+		resolve: {
+			modules: [
+				__dirname,
+				'node_modules'
+			]
+		},
+		module: {
+			rules: [
+				{
+					test: /.js?$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/
+				}
+			]
+		},
+		plugins: [
+			new webpack.DefinePlugin( {
+				'process.env.NODE_ENV': JSON.stringify( process.env.NODE_ENV || 'development' )
+			} )
+		]
+	},
+	{
+		entry: './public-assets/js/transcoder.js',
+		output: {
+			filename: './public-assets/js/build/transcoder.min.js',
+			path: __dirname
+		}
+	}
+];
+
+if (process.env.NODE_ENV === 'production') {
+	for (var moduleConfig of module.exports) {
+		moduleConfig.optimization = {
+			minimize: true,
+			minimizer: [
+				new TerserPlugin({
+					extractComments: false,
+					terserOptions: {
+						ecma: 8,
+						compress: {
+							warnings: false
+						},
+						output: {
+							comments: false
+						},
+						sourceMap: true
+					}
+				})
+			]
+		};
+	}
+}
